ca74ec858666564b9b77f4ce63a4700648a51494
Thekgiga <thekgiga@gmail.com>
2015-04-06 23:39:11 +0200

    Uradjena dokumentacija koda za klasu Twitter.

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -3,14 +3,33 @@ package com.twitter;
 import java.util.LinkedList;
 import com.twitter.poruke.TwitterPoruka;
 
+/**
+* Osnovna klasa za platformu sajta Twitter.com
+* @author Thekgiga
+* @version 1.9
+*
+*/
 public class Twitter {
-
+	
+	/**
+	* Lista poruka koje unose korisnici.
+	*/
 	private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
 
+	/**
+	* Metoda vraca sve poruke koje su korisnici uneli.
+	* @return poruke koje su unete od strane korisnika
+	*/
 	public LinkedList<TwitterPoruka> vratiSvePoruke() {
 		return poruke;
 	}
 
+	/**
+	* Metoda unosi novu poruku, koju je napisao korisnik, u listu.
+	*
+	* @param korisnik Korisnik koji salje poruku
+	* @param poruka Poruka koju je korisnik uneo.
+	*/
 	public void unesi(String korisnik, String poruka) {
 		// Pravi se nova poruka i puni podacima.
 		TwitterPoruka tp = new TwitterPoruka();
@@ -20,6 +39,21 @@ public class Twitter {
 		poruke.addLast(tp);
 	}
 
+	/**
+	* Metoda prolazi kroz listu poruka i sve one koje sadrze dati tag ubacuje u niz, sve do maksimalnog broja poruka koji je zadat.
+	* Ova metoda zatim vraca novonastali niz poruka.
+	*
+	* @param maxBroj Maksimalan broj poruka koje se ubacuju u niz
+	* @param tag Tag na osnovu koga se vrsi selekcija poruka koje se ubacuju u niz
+	* @return vratiPoruke vraca niz poruka izdvojenih na osnovu izvrsene pretrage
+	*
+	* @throws java.lang.RuntimeException ako je tag 
+	*
+	* <ul>
+	* <li>null ili </li>
+	* <li>prazan String</li>
+	* </ul>
+	*/
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
 		if (tag == null || tag.isEmpty())
 			throw new RuntimeException("Morate uneti tag");
