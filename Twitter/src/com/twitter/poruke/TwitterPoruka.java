64f203db035da97180ee2b611fc689627952056f
Thekgiga <thekgiga@gmail.com>
2015-04-06 21:43:07 +0200

    Napravljena JavaDoc dokumentacija za sve atribute i metode klase TwitterPoruka.

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,15 +1,35 @@
 package com.twitter.poruke;
 
+/**
+ * Predstavlja TwitterPoruku.
+ * @author Thekgiga
+ *@version 1.0
+ */
 public class TwitterPoruka {
 	
-	
+	/**
+	 * Predstavlja korisnika Twitter-a, koji je poslao tweet.
+	 */
 	private String korisnik;
+	
+	/**
+	 * Tekst poslate poruke.
+	 */
 	private String poruka;
 	
+	/**
+	 * metoda vraca ime korisnika, tj vednost atributa korisnik.
+	 * @return ime korisnika kao string.
+	 */
 	public String getKorisnik() {
 		return korisnik;
 	}
 	
+	/**
+	 * Metoda postavlja korisnika na unetu vrednost.
+	 * @param korisnik ime korisnika.
+	 * @throws java.lang.RuntimeException ako je uneto ime korisnika null ili prazan string.
+	 */
 	public void setKorisnik(String korisnik) {
 		if (korisnik==null || korisnik.equals(""))
 			throw new RuntimeException(
@@ -17,10 +37,23 @@ public class TwitterPoruka {
 		this.korisnik = korisnik;
 	}
 	
+	/**
+	 * metoda vraca tekst poruke, tj vednost atributa poruka.
+	 * @return tekst poruke kao String.
+	 */
 	public String getPoruka() {
 		return poruka;
 	}
 	
+	/**
+	 * metoda postavlja poruku na unetu vrednost.
+	 * @param poruka uneta vrednost poruke.
+	 * @throws java.lang.RuntimeException ako je uneta vrednost poruke 
+	 * <ul> 
+	 * <li>null;</li>
+	 * <li>duza od 140 karaktera.</li>
+	 * </ul>
+	 */
 	public void setPoruka(String poruka) {
 		if (poruka==null || poruka.length()>140)
 			throw new RuntimeException(
@@ -28,6 +61,9 @@ public class TwitterPoruka {
 		this.poruka = poruka;
 	}
 	
+	/**
+	 * metoda daje informaciju o korisniku i poruci koju je napisao.
+	 */
 	public String toString(){
 		return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
 	}
